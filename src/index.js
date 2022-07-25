bce671e858f0af4080c602b0158893bc955be561
Rachel Katz <rachel.katz@flatironschool.com>
2022-07-25 09:08:22 -0400

    event listeners and fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+const form = document.querySelector('.add-toy-form');
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  let name = e.target.name.value;
+  let image = e.target.image.value;
+  let likes = 0;
+  addToDom(name, image, likes);
+})
+
+let addToDom = function(name, image, likes){
+  console.log(name, image, likes);
+}
+
+fetch('http://localhost:3000/toys')
+.then(res => res.json())
+.then(data => {
+  data.forEach((el, i) => {
+    addToDom(el.name, el.image, el.likes);
+  })
+})
+.catch(err => console.log(err));
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
